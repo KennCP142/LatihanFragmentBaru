27dbb24c7ca1e87a16e0b0f11f0a47fe17060a14
HedgeHawk808 <kenncahyaputra142@gmail.com>
2024-10-30 19:17:34 +0700

    Initial commit

---
+++ app/src/main/java/com/c14220001/latFragment/MainActivity.kt
@@ -0,0 +1,44 @@
+package com.c14220001.latFragment
+
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, FragmentOne())
+                .commit()
+        }
+
+        findViewById<Button>(R.id.button_fragment_one).setOnClickListener {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, FragmentOne())
+                .commit()
+        }
+
+        findViewById<Button>(R.id.button_fragment_two).setOnClickListener {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, FragmentTwo())
+                .commit()
+        }
+
+        findViewById<Button>(R.id.button_fragment_three).setOnClickListener {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, FragmentThree())
+                .commit()
+        }
+    }
+}
